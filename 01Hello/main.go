6b28e3e36d23851ce2fdf6ed02fa2481b691ce5f
Rushabh Gopal Aradwad <aradwadrushabh@gmail.com>
2024-12-11 19:00:45 +0530

    Starting Go Lang

---
+++ 01Hello/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello Rushabh")
+}
+
+/*
+step 1 : go mod init hello --- initialization
+step 2 : write functional code
+step 3 : go run main.go  --- to execute file
+*/
